72e89862abd1569c7648793bfc994a3e2f89289d
jakevh117 <jakevh117@gmail.com>
2022-10-24 10:52:25 +1000

    setting up shared mem in sim

--- simulation.c
+++ simulation.c
@@ -10,7 +10,28 @@
 int shm_fd;
 volatile void *shm;
 
+#define LEVELS 5
+#define ENTRANCES 5
+#define EXITS 5
+
+struct boomgate {
+	pthread_mutex_t m;
+	pthread_cond_t c;
+	char s;
+};
+struct parkingsign {
+	pthread_mutex_t m;
+	pthread_cond_t c;
+	char display;
+};
+
+struct tempnode {
+	int temperature;
+	struct tempnode *next;
+};
+
+
 int main(int argc, int * argv){
-    shm_fd = shm_open("PARKING", O_RDWR, 0);
+    shm_fd = shm_open("PARKING", O_CREAT, 0);
 	shm = (volatile void *) mmap(0, 2920, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 }
