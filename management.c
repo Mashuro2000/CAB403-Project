77a7a199c0ce566b1e5d1eb2024575a634c20e02
jakevh117 <jakevh117@gmail.com>
2022-10-24 12:16:20 +1000

    created management file

---
+++ management.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "parking.h"
+
+int shm_fd;
+volatile void *shm;
+char allowed_cars[NUM_ALLOW_CARS][PLATESIZE];
+
+// Managment system can only read sensors and keep track of cars that way, along with issuing commands to
+// boom gates and display screens, and time keeping and generating billing
+
+
+int main(int argc, int *argv){
+    //read in license plate file
+
+    // open shared memory
+    shm_fd = shm_open("PARKING", O_RDWR, 0);
+	shm = (volatile void *) mmap(0, 2920, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+}
--- simulation.c
+++ simulation.c
@@ -10,20 +10,27 @@
 
 int shm_fd;
 volatile void *shm;
+char allowed_cars[NUM_ALLOW_CARS][PLATESIZE]
 
-struct car{
+//local linked list struct for cars within each level or waiting at entrance/exit
+struct cars{
     char lplate[6];
     struct car *next;
 };
 
+
+
 // cars simulated locally in simulation file, but update LPR values
 // cars update LPR value of entrance, updates LPR of level it goes to, waits updates LPR of level it leaves, updates LPR of its chosen exit
+void generate_car(){
 
+}
 
 
 int main(int argc, int * argv){
+
+
     shm_fd = shm_open("PARKING", O_CREAT, 0);
 	shm = (volatile void *) mmap(0, 2920, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    int i = PLATESIZE;
 }
