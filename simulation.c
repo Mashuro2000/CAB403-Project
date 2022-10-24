e1897838e53dcb26c0dba2c16c33892a8e6671fb
jakevh117 <jakevh117@gmail.com>
2022-10-24 12:23:08 +1000

    started on cars

--- simulation.c
+++ simulation.c
@@ -8,21 +8,35 @@
 #include <fcntl.h>
 #include "parking.h"
 
+
 int shm_fd;
 volatile void *shm;
-char allowed_cars[NUM_ALLOW_CARS][PLATESIZE]
+// needed so that a car can be generated that is allowed
+char allowed_cars[NUM_ALLOW_CARS][PLATESIZE];
 
 //local linked list struct for cars within each level or waiting at entrance/exit
+// not in shared mem
 struct cars{
     char lplate[6];
     struct car *next;
 };
 
+// read in license plates from file
+void read_allowed_plates(){
 
+}
 
 // cars simulated locally in simulation file, but update LPR values
 // cars update LPR value of entrance, updates LPR of level it goes to, waits updates LPR of level it leaves, updates LPR of its chosen exit
+
+// generate car 50% chance of being on allowed list
+// add to linked list of car queue at one of the entrances
 void generate_car(){
+
+}
+
+// move car, updates LPR of place where car moves to
+void move_car(struct LPR *leaving, struct LPR *entering){
 
 }
 
