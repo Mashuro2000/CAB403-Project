df370d44151f6b3ca706682e0b7bf536f434d005
jakevh117 <jakevh117@gmail.com>
2022-10-24 10:50:19 +1000

    setting up shared mem in sim

--- simulation.c
+++ simulation.c
@@ -1,1 +1,16 @@
-//test2
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int shm_fd;
+volatile void *shm;
+
+int main(int argc, int * argv){
+    shm_fd = shm_open("PARKING", O_RDWR, 0);
+	shm = (volatile void *) mmap(0, 2920, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+}
